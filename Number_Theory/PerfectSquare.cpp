9cd5cd092b369f58e766fc670743db72f68ec37f
Daksh Dixit <dakshdixit183@gmail.com>
2025-03-08 11:25:56 +0530

    PerfectSquare

---
+++ Number_Theory/PerfectSquare.cpp
@@ -0,0 +1,150 @@
+/*Daksh Dixit*/
+#pragma GCC optimize("O3,unroll-loops")
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<iostream>
+#include<istream>
+#include<fstream>
+ 
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long
+const int mod = 1000000007;
+// const int mod = 998244353;
+#define inf 1000000000000000005
+#define endl "\n"
+#define pb push_back
+#define X first
+#define Y second
+#define yes cout << "YES" <<endl
+#define no cout << "NO" <<endl
+#define PI 3.141592653589793238462
+#define set_bits(x) __builtin_popcountll(x)
+#define all(x) (x).begin(), (x).end()
+#define fr(i,a,b) for(int i = a; i < b; i++)
+#define fo(i,a,b) for(int i=a;i<=b;i++)
+#define ms(arr, v) memset(arr, v, sizeof(arr))
+#define ai(a, n) for (int ele = 0; ele < n; ele++) cin >> a[ele];
+#define ain(a, lb, rb) for (int ele = lb; ele <= rb; ele++) cin >> a[ele];
+#define ao(a, n) {for (int ele = 0; ele < (n); ele++) { if (ele) cout << " "; cout << a[ele]; } cout << '\n';}
+#define aout(a, lb, rb) {for (int ele = (lb); ele <= (rb); ele++) { if (ele > (lb)) cout << " "; cout << a[ele]; } cout << '\n';}
+#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vpi;
+template<class T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; //greater<int> for descending set, also less_equal<int> which is ascending multiset 
+// find_by_order(k)  returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
+//DEBUG SECTION
+void __print(long x) { cerr << x; }void __print(long long x) { cerr << x; }void __print(unsigned x) { cerr << x; }void __print(unsigned long x) { cerr << x; }void __print(unsigned long long x) { cerr << x; }void __print(float x) { cerr << x; }void __print(double x) { cerr << x; }void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }void __print(const char *x) { cerr << '\"' << x << '\"'; }void __print(const string &x) { cerr << '\"' << x << '\"'; }void __print(bool x) { cerr << (x ? "true" : "false"); }
+template <typename T, typename V>void __print(const pair<T, V> &x){cerr << '{';__print(x.first);cerr << ',';__print(x.second);cerr << '}';}
+template <typename T>void __print(const T &x){int f = 0;cerr << '{';for (auto &i : x){cerr << (f++ ? "," : ""), __print(i);}cerr << "}";}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V> void _print(T t, V... v){__print(t);if (sizeof...(v)){cerr << ", ";}_print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...)cerr << #x << " = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+//DEBUG SECTION
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int getRandomNumber(int l, int r) {return uniform_int_distribution<int>(l, r)(rng);} 
+int MSB(int n){return 63-__builtin_clzll(n);}
+int getIthbit(int n, int i){return ( n & ( 1 << i) ) == 0 ? 0 : 1;}
+void setIthBit(int &n, int i){ n = n | (1 << i);}
+void clearIthBit(int &n, int i){ n = n & ( ~(1 << i));}
+int gcd(int a, int b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+int pwr(int a, int b) {a %= mod; int res = 1; while (b > 0) {if (b & 1) res = (res * a) % mod; a = (a * a) % mod; b >>= 1;} return res;}
+void extendgcd(int a, int b, int*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); int x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+int mminv(int a){int arr[3];extendgcd(a, mod, arr); if(arr[2]!=1){return -1;}arr[0] =(arr[0]%mod + mod)%mod ;return arr[0];} //Modulo Inverse Exist only if gcd(a,m)=1;
+int inv(int i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+int mminvprime(int a){int ans = pwr(a, mod - 2); ans = (ans%mod+mod)%mod; return ans;} //If a,m are Coprime
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+void google(int t) {cout << "Case #" << t << ": ";}
+int mod_add(int a, int b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+int mod_mul(int a, int b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+int mod_sub(int a, int b) {a = a % mod; b = b % mod; return (((a - b) % mod) + mod) % mod;}
+int mod_div(int a, int b) {a = a % mod; b = b % mod; return (mod_mul(a, mminvprime(b)) + mod) % mod;}  //only for prime m
+int phin(int n) {int number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (int i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+int ceil_div(int a, int b) {return a % b == 0 ? a / b : a / b + 1;}
+int ceil_rem(int a, int b) {return a%b==0 ? b : a%b;}
+
+class CMP{
+public:
+    bool operator()(pi &a , pi &b) //For min priority_queue .
+    {    return ! ( a.X < b.X || ( a.X==b.X && a.Y <= b.Y ));   }
+    // priority_queue<pi, vector<pi>, CMP> minPQ;
+};
+void add(map<int,int> &m, int x,int cnt=1){
+    auto jt=m.find(x);
+    if(jt==m.end()){m.insert({x,cnt});} 
+    else{jt->Y+=cnt;}
+}
+void del(map<int,int> &m, int x,int cnt=1){
+    auto jt=m.find(x);
+    if(jt->Y<=cnt){m.erase(jt);}
+    else{jt->Y-=cnt;}
+}
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
+const int N = 5e5+5;
+bool isPerfectSquare(int n){
+    if(n==1){
+        return true;
+    }
+    int l = 1, r = N;
+    while(r-l>1){
+        int mid = (l+r)/2;
+        if(mid*mid<=n){
+            l = mid;
+        }
+        else{
+            r = mid;
+        }
+    }
+    return l*l==n;
+}
+
+void Solve(){
+    for(int i=0;i<N;i++){
+        if(isPerfectSquare(i)){
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);;
+    auto start1 = high_resolution_clock::now();
+    // #ifndef ONLINE_JUDGE
+    // freopen("shell.in", "r", stdin);
+    // freopen("shell.out", "w", stdout);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    int t=1;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        // cout << "Case #" << t << ": ";
+        Solve();
+    }
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+    #ifndef ONLINE_JUDGE
+    cerr << "Time: " << duration . count() / 1000 << endl;
+    #endif
+    return 0;
+}
